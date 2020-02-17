cc89f79c797ded5781e961a030b8ea45fd2e8e70
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-02-17 16:52:49 +0600

    n

--- home/urls.py
+++ home/urls.py
@@ -7,8 +7,9 @@ from django.conf import settings
 urlpatterns = [
     path('types/', views.TypeView().as_view()),
     path('images/', views.ImageView().as_view()),
+    path('address', views.AddressView().as_view()),
     path('apartment/', views.ApartmentView().as_view()),
     path('apartment/<int:pk>/', views.ApartmentDetail().as_view()),
     path('announcements/', views.AnnouncementView().as_view()),
-    path('ap-type/<int:pk>/', views.ApartmentsTypeView.as_view()),
+    path('apartment-type/<int:pk>/', views.ApartmentsTypeView.as_view()),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
