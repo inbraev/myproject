e3e3c857313752889d22e89dde7c0043dced518f
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-02-17 16:56:15 +0600

    n

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('registration', RegistrationView().as_view()),
-    path('user-update', UserDetail().as_view()),
+    path('users/<int:pk>', UserDetail().as_view()),
+    path('users', UserView().as_view()),
 ]
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
