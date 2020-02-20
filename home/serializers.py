325ee0a34f4b1103f2be5274549cc2093e7b2d08
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-02-21 02:19:22 +0600

    preview

--- home/serializers.py
+++ home/serializers.py
@@ -32,7 +32,6 @@ class AddressSerializer(serializers.ModelSerializer):
 
 
 class ApartmentSerializer(serializers.ModelSerializer):
-    type = serializers.CharField(source='type.__str__')
     owner = serializers.CharField(source='owner.__str__')
     address = AddressSerializer()
     images = ImageSerializer(many=True)
