e9ea2c084fd52cc78e2d9e3150dff38394d79303
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-03-03 21:00:52 +0600

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import *
 
 admin.site.register(Type)
+admin.site.register(Room)
+admin.site.register(Floor)
+admin.site.register(Series)
+admin.site.register(Construction)
+admin.site.register(State)
+admin.site.register(Area)
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(District)
+admin.site.register(Location)
+admin.site.register(Detail)
+admin.site.register(Currency)
+admin.site.register(Role)
+admin.site.register(Rent)
+admin.site.register(Contact)
+admin.site.register(Comment)
+admin.site.register(Booking)
 admin.site.register(Image)
 admin.site.register(Apartment)
-admin.site.register(Address)
-
-
-
