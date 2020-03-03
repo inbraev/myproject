bd6c3ccb39ce0db6165c4138b60501175b499709
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-03-03 17:02:25 +0600

    New add serializer

--- home/serializers.py
+++ home/serializers.py
@@ -9,6 +9,109 @@ class TypeSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'room')
+
+
+class FloorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Floor
+        fields = ('id', 'floor')
+
+
+class ConstructionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Construction
+        fields = ('id', 'name')
+
+
+class SeriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Series
+        fields = ('id', 'name')
+
+
+class StateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = State
+        fields = ('id', 'name')
+
+
+class AreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Area
+        fields = ('id', 'total_area', 'living_area')
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ('id', 'name')
+
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = ('id', 'name', 'country')
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ('id', 'name', 'region')
+
+
+class DistrictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = District
+        fields = ('id', 'name', 'city')
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ('id', 'country', 'region', 'city', 'district', 'street', 'house_number', 'latitude', 'longitude')
+
+
+class DetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Detail
+        fields = ('id', 'furniture', 'heat', 'gas', 'electricity', 'internet', 'phone', 'elevator', 'security',
+                  'parking')
+
+
+class CurrencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = ('id', 'name')
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('id', 'name')
+
+
+class RentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rent
+        fields = ('id', 'name')
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ('id', 'role', 'phone', 'name', 'surname')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'name_of_publication', 'text_of_publication', 'date_of_publication', 'owner')
+
+
 class ImageSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(max_length=None, required=False)
 
@@ -17,52 +120,49 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = ('id', 'image', 'apartment')
 
 
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ('id', 'apartment', 'arrival_date', 'departure_date')
+
+
+class ApartmentSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
+    location = LocationSerializer()
+    images = ImageSerializer(many=True)
+    area = AreaSerializer()
+    contact = ContactSerializer()
+    detail = DetailSerializer()
+
+    class Meta:
+        model = Apartment
+        fields = ('id', 'type', 'room', 'floor', 'area', 'series', 'construction_type', 'state',
+                  'detail', 'location', 'rental_period', 'price', 'currency', 'preview_image', 'description',
+                  'pub_date', 'images', 'contact', 'owner')
+
+    def create(self, validated_data):
+        location_data = validated_data.pop('location')
+        images_data = validated_data.pop('images')
+        area_data = validated_data.pop('area')
+        contact_data = validated_data.pop('contact')
+        detail_data = validated_data.pop('detail')
+
+        area = Area.objects.create(**area_data)
+        location = Location.objects.create(**location_data)
+        contact = Contact.objects.create(**contact_data)
+        detail = Detail.objects.create(**detail_data)
+
+        apartment = Apartment.objects.create(area=area, location=location, detail=detail, contact=contact, **validated_data)
+
+        for image_data in images_data:
+            Image.objects.create(**image_data)
+        return apartment
+
+
 class ApartmentsTypeSerializer(serializers.ModelSerializer):
     types = TypeSerializer(many=True)
 
     class Meta:
         model = Apartment
         fields = ('id', 'types',)
-
-
-class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Address
-        fields = ('id', 'house_number', 'street', 'city', 'postcode', 'country', 'country_code')
-
-
-class ApartmentSerializer(serializers.ModelSerializer):
-    address = AddressSerializer()
-    images = ImageSerializer(many=True)
-
-    class Meta:
-        model = Apartment
-        fields = (
-            'id', 'type', 'room', 'floor', 'square', 'preview_image', 'date_of_arrival', 'date_of_departure', 'price',
-            'description',
-            'status', 'pub_date', 'images', 'owner', 'latitude', 'longitude', 'address')
-
-    def create(self, validated_data):
-        address_data = validated_data.pop('address')
-        images_data = validated_data.pop('images')
-
-        address = Address.objects.create(**address_data)
-        apartment = Apartment.objects.create(address=address, **validated_data)
-
-        for image_data in images_data:
-            Image.objects.create(apartment=apartment, **image_data)
-        return apartment
-
-
-class AnnouncementSerializer(serializers.ModelSerializer):
-    type = serializers.CharField(source='type.__str__')
-    address = AddressSerializer(many=False)
-    owner = serializers.CharField(source='owner.__str__')
-    images = ImageSerializer(many=True)
-
-    class Meta:
-        model = Apartment
-        fields = ('id', 'type', 'room', 'floor', 'address', 'square', 'preview_image', 'date_of_arrival', 'date_of_departure',
-                  'price', 'description', 'status', 'latitude', 'longitude', 'pub_date', 'images', 'owner')
-
-
