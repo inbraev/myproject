8ad27a3552c80a7ff320e1afb2363f60e6e0937e
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-03-03 20:53:27 +0600

    Without permissions

--- home/views.py
+++ home/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.response import Response
@@ -6,10 +5,117 @@ from .permissions import IsOwner
 from .serializers import *
 
 
-
-class TypeView(generics.ListAPIView):
+class TypeView(generics.ListCreateAPIView):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class RoomView(generics.ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class FloorView(generics.ListCreateAPIView):
+    queryset = Floor.objects.all()
+    serializer_class = FloorSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class ConstructionView(generics.ListCreateAPIView):
+    queryset = Construction.objects.all()
+    serializer_class = ConstructionSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class SeriesView(generics.ListCreateAPIView):
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class StateView(generics.ListCreateAPIView):
+    queryset = State.objects.all()
+    serializer_class = StateSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class AreaView(generics.CreateAPIView):
+    queryset = Area.objects.all()
+    serializer_class = AreaSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class CountryView(generics.ListCreateAPIView):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class RegionView(generics.ListCreateAPIView):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class CityView(generics.ListCreateAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class DistrictView(generics.ListCreateAPIView):
+    queryset = District.objects.all()
+    serializer_class = DistrictSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class LocationView(generics.CreateAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class DetailView(generics.CreateAPIView):
+    queryset = Detail.objects.all()
+    serializer_class = DetailSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class CurrencyView(generics.ListCreateAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class RoleView(generics.ListCreateAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class RentView(generics.ListCreateAPIView):
+    queryset = Rent.objects.all()
+    serializer_class = RentSerializer
+    permission_classes = (permissions.IsAdminUser,)
+
+
+class ContactView(generics.CreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class CommentView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class BookingView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
     permission_classes = (permissions.AllowAny,)
 
 
@@ -19,24 +125,24 @@ class ImageView(generics.ListCreateAPIView):
     permission_classes = (permissions.AllowAny,)
 
 
-class AnnouncementView(generics.ListAPIView):
-    serializer_class = AnnouncementSerializer
-    queryset = Apartment.objects.all()
-    permission_classes = (permissions.AllowAny,)
-
-
 class ApartmentView(generics.CreateAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
     permission_classes = (permissions.AllowAny,)
 
-#     def perform_create(self, serializers):
-#         serializers.save(owner=self.request.user)
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
 
 
 class ApartmentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
+    permission_classes = (permissions.AllowAny, IsOwner)
+
+
+class ApartmentListView(generics.ListAPIView):
+    serializer_class = ApartmentsSerializer
+    queryset = Apartment.objects.all()
     permission_classes = (permissions.AllowAny,)
 
 
@@ -51,19 +157,3 @@ class ApartmentsTypeView(generics.RetrieveAPIView):
         types = Apartment.objects.filter(type_id=instance.id)
         serializer = ApartmentSerializer(types, many=True)
         return Response(serializer.data)
-    
-    
-class AddressView(generics.CreateAPIView):
-    queryset = Address.objects.all()
-    serializer_class = AddressSerializer
-    permission_classes = (permissions.AllowAny,)
-    
-    
-class ActiveApartmentView(generics.ListAPIView):
-    queryset = Apartment.objects.filter(status=True)
-    serializer_class = AnnouncementSerializer
-    permission_classes = (permissions.AllowAny,)
-    
-
-
-
