b6d2b593a9e412ac47f897de68d1770cad2b7d5d
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-03-03 21:05:03 +0600

    Added JWT

--- user/serializers.py
+++ user/serializers.py
@@ -1,17 +1,21 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 
 from .models import *
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    token = serializers.CharField(max_length=255, read_only=True)
+
     class Meta:
         model = User
-        fields = ('email', 'username', 'password', 'phone', 'name', 'surname')
-        extra_kwargs = {'password': {'write_only': True}}
-
-
-class RegistrationSerializer(serializers.ModelSerializer):
+        fields = ['email', 'username', 'password', 'phone', 'name', 'surname', 'token']
 
     def create(self, validated_data):
         user = get_user_model().objects.create(
@@ -25,7 +29,54 @@ class RegistrationSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True)
+
     class Meta:
         model = User
-        fields = ['email', 'username', 'password', 'phone', 'name', 'surname']
-        
+        fields = ('id', 'email', 'username', 'password', 'phone', 'name', 'surname')
+        extra_kwargs = {'password': {'write_only': True}}
+        read_only_fields = ('token',)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+
+        for (key, value) in validated_data.items():
+            setattr(instance, key, value)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=255, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        if username is None:
+            raise serializers.ValidationError('An username is required to log in.')
+
+        if password is None:
+            raise serializers.ValidationError('A password is required to log in.')
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError('A user with this email and password was not found.')
+
+        if not user.is_active:
+            raise serializers.ValidationError('This user has been deactivated.')
+
+        return {
+            'username': user.username,
+            'token': user.token
+        }
