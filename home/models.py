444680314d24fe8426a0e9a5782a9123cec2ced6
inbraev <ibraev.nazar@yandex.ru>
2020-04-14 03:39:05 +0600

    removed name attribute from save method of apartment

--- home/models.py
+++ home/models.py
@@ -236,7 +236,7 @@ class Apartment(models.Model):
         full_page = requests.get(DOLLAR_SOM, headers=headers)
         soup = BeautifulSoup(full_page.content, 'html.parser')
         convert = soup.findAll("h2")
-        if self.currency.name == "сом":
+        if self.currency == "сом":
             self.another_price = round(float(self.price) / float(convert[1].text), 2)
         else:
             self.another_price = round(float(convert[1].text) * float(self.price), 2)
