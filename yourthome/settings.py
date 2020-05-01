8e1cc8cdfe04fb194e8cff5015a0a1c1234eed01
OLOLO6 <56263303+OLOLO6@users.noreply.github.com>
2020-05-01 18:57:35 +0600

    added email configs

--- yourthome/settings.py
+++ yourthome/settings.py
@@ -215,3 +215,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'YourtHome Team <noreply@yourthome.com>'
